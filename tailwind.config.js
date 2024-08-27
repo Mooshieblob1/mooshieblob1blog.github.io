8c39e4f549aa9a188429fa414ad625386a4ffdd4
Mooshieblob <mooshieblob@pm.me>
2024-08-27 12:45:32 +0000

    added tailwind css functionality and updated paths

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",           // Include the root index.html file
+    "./assets/css/style.css", // Include the CSS file
+    "./assets/js/script.js",  // Include the JS file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
