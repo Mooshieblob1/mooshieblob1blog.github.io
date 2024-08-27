b446ec6c77e9a3e9abd27b6ac6008f1113622578
Mooshieblob1 <116948241+Mooshieblobs@users.noreply.github.com>
2024-08-28 00:01:47 +0800

    Update Prettier configuration and Tailwind CSS file inclusion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",           // Include the root index.html file
+    "./index.html", // Include the root index.html file
     "./assets/css/style.css", // Include the CSS file
-    "./assets/js/script.js",  // Include the JS file
+    "./assets/js/script.js", // Include the JS file
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
